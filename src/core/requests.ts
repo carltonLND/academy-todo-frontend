55939c0531c38b3c829b95f1d8ce3a57c5e32c85
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-08-02 16:04:48 +0100

    Add production url, temporarily remove catching axios errors

--- src/core/requests.ts
+++ src/core/requests.ts
@@ -1,20 +1,15 @@
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 
 export interface Task {
   task: string;
 }
 
-axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://todo-backend-ggbb.onrender.com"
+    : "http://localhost:4000";
 
 export async function getTasks(): Promise<Task[]> {
-  try {
-    const response = await axios.get("/tasks");
-    return response.data;
-  } catch (_e) {
-    const e = _e as AxiosError;
-    if (e.code && e.code !== "ECONNABORTED") {
-      console.log("AXIOS ERROR", e.code);
-    }
-    return [];
-  }
+  const response = await axios.get("/tasks");
+  return response.data;
 }
