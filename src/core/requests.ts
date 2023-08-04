42a2734237a1a6e34fec2ace68295f98713f1b6e
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-08-04 14:24:40 +0100

    Formatting

--- src/core/requests.ts
+++ src/core/requests.ts
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-const taskServer = axios.create({
-  baseURL: import.meta.env.VITE_SERVER_URL,
-});
-
 export interface TaskCandidate {
   content: string;
 }
@@ -11,6 +7,10 @@ export interface TaskCandidate {
 export interface Task extends TaskCandidate {
   id: number;
 }
+
+const taskServer = axios.create({
+  baseURL: import.meta.env.VITE_SERVER_URL,
+});
 
 export async function getTasks(): Promise<Task[]> {
   const response = await taskServer.get("/tasks");
